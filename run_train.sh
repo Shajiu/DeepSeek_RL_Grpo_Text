39914e621089c1217c3a1f3e9b1348797bea16eb
shajiu <31726161+Shajiu@users.noreply.github.com>
2025-03-08 00:06:31 +0800

    Update run_train.sh

--- run_train.sh
+++ run_train.sh
@@ -1,1 +1,1 @@
-python ./scripts/train.py --model_name_or_path "/root/paddlejob/workspace/env_run/output/per_models/Qwen2_5_1_5B_Instruct"
+python ./scripts/train.py --model_name_or_path "output/per_models/Qwen2.5-7B-Instruct"
