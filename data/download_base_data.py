e6668820c520e7aefa9cd955b2ed46c654d1b70f
shajiu <31726161+Shajiu@users.noreply.github.com>
2025-03-07 23:58:20 +0800

    Update download_base_data.py

--- data/download_base_data.py
+++ data/download_base_data.py
@@ -3,7 +3,7 @@ import json
 
 data = load_dataset("openai/gsm8k", 'main')
 for tpye in ["train","test"]:
-    file_output=open(f"/root/paddlejob/workspace/env_run/output/DeepSeek_RL_Grpo/data/{tpye}.jsonl",'w',encoding="utf-8")
+    file_output=open(f"output/DeepSeek_RL_Grpo/data/{tpye}.jsonl",'w',encoding="utf-8")
     for example in data[tpye]:
         print("*"*100)
         file_output.write(json.dumps(example,ensure_ascii=False)+"\n")
